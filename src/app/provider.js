f108a3ab416d8734fd9b97f098aa7c13de9d354f
Kelveen Raj <kelveenrajv@gmail.com>
2025-01-21 00:07:08 +0800

    remove default theme

--- src/app/provider.js
+++ src/app/provider.js
@@ -4,7 +4,6 @@ import { ThemeProvider } from "next-themes";
 import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
 
 export default function RootLayout({ children }) {
-  localStorage.setItem("theme", "light"); // set default theme
   return (
     <ChakraProvider value={defaultSystem}>
       <ThemeProvider attribute="class" disableTransitionOnChange>
